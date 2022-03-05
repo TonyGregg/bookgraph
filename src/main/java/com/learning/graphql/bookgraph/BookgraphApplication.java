98354492d395356a2787a98826f5ad55451b2dae
Antony G <antonygenil@gmail.com>
2022-03-05 12:07:36 -0600

    Initial commit

---
+++ src/main/java/com/learning/graphql/bookgraph/BookgraphApplication.java
@@ -0,0 +1,13 @@
+package com.learning.graphql.bookgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookgraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookgraphApplication.class, args);
+	}
+
+}
